68891a508042a16128e15f64714c913a906f937c
Sl0niQ <31eriflleh@gmail.com>
2025-11-13 23:15:35 +0700

    main: реализован обмен сообщениями через телеграм-бот

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 # TELEGRAM_BOT_NAME 		= '6_hw_api_mipt'
 # TELEGRAM_BOT_USER_NAME 	= 'hw_api_mipt_bot'
-TELEGRAM_TOKEN = '' 	
+
+POLL_INTERVAL = 30 		# время ожидания новой команды/команд в текущем запросе
+PAUSE = 15				# интервал между запросами к боту	
+TIME_OUT = 35			# максимальное время ожидания ответа от бота
+LIMIT = 100 			# предельное количество сообщений в одном запросе
 
 # Параметры адаптера
 POOL_CONNECTIONS = 1 	# количество хостов в пуле
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import requests
 import threading
 from time import sleep
 from requests.adapters import HTTPAdapter
-from config import TELEGRAM_TOKEN, POOL_CONNECTIONS, POOL_MAXSIZE, MAX_RETRIES, POOL_BLOCK
+from config import POOL_CONNECTIONS, POOL_MAXSIZE, MAX_RETRIES, POOL_BLOCK, POLL_INTERVAL, PAUSE, TIME_OUT, LIMIT
 
 ############################ Test code ->
 """
@@ -19,86 +19,121 @@ from config import TELEGRAM_TOKEN, POOL_CONNECTIONS, POOL_MAXSIZE, MAX_RETRIES, 
 ############################ Test code <-
 
 class TelegramBot:
-	def __init__(self, token, poll_interval=45):
+	#---------в разработке
+	def __init__(self, token):
 		"""Конструктор"""		
 		self.token = token 					# токен для соединения с телеграм-ботом
 		self.exit_flag = False 				# флаг завершения работы скрипта		
-		self.poll_interval = poll_interval 	# интервал опроса API телеграм-бота
-		self.last_message_id = 0 			# id последней обработанной команды
+		self.req_pause = PAUSE 				# интервал опроса API телеграм-бота
+		self.last_message_id = -1			# id последней обработанной команды
 
-	def wait_for_exit(self): #-->ok
-		"""Обработка команды остановки приложения"""
+		self.session = requests.session() 
+		adapter = HTTPAdapter(
+			pool_connections = POOL_CONNECTIONS,
+			pool_maxsize = POOL_MAXSIZE,
+			max_retries = MAX_RETRIES,
+			pool_block = POOL_BLOCK
+		)
+		self.session.mount("https://", adapter)
+
+	#---------ok
+	def wait_for_exit(self):
+		""" Обработка команды остановки приложения """
 		while not self.exit_flag:
 			if input().strip().lower() == 'exit':
 				self.exit_flag = True
 				print("Завершение обработки текущих запросов...")
 				break
 
-	def pause(self): #-->ok
-		"""Пауза между запросами к API"""
-		for _ in range(self.poll_interval):
+	#---------ok ?нужно?
+	def pause(self): 
+		""" Пауза между запросами к API """
+		for _ in range(self.req_pause):
 			if self.exit_flag:
 				break
 			sleep(1)
-	"""
-	def find_last_message_id(self, session, url):
-		#latest_message = max(updates, key=lambda x: x['message']['message_id'])
-		return None
-	"""
 
-	def single_message_processing():
-		"""
-		обработка одной команды:
-		- извлечение команды и id;
-		- сохранение id в self.last_message_id если значение больше;
-		- анализ команды и выполнение соответствующего действия
-		"""
-		pass
+	#---------в разработке
+	def send_result(self, chat_id, data):
+		try:
+			self.session.request(
+				"POST", 
+				f"https://api.telegram.org/bot{self.token}/sendMessage", 
+				json={'chat_id': chat_id, 'text': data}, 
+				timeout=TIME_OUT
+			)			
+		except requests.exceptions.RequestException as e:
+			print(f"Не удалось отправить сообщение: {e}")
 
-	def messages_processing():
+	#---------ok
+	def get_updates(self, params):
+		"""	Чтение новых сообщений """
+		if self.last_message_id > -1:
+			params['offset'] = self.last_message_id + 1 # для чтения только необработанных команд
+		response = self.session.request(
+			"GET", 
+			f"https://api.telegram.org/bot{self.token}/getUpdates", 
+			params=params, 
+			timeout=TIME_OUT
+		)				
+		return response.json()['result']
+
+	#---------в разработке
+	def messages_processing(self, updates):
 		"""
 		обработка пакета:
-		- цикл по елементам пакета
+		- цикл по элементам пакета
 		- обработка отдельных элементов
-		"""
-		pass
-
+		"""	
+		for update in updates:
+			print(f"update_id: {update['update_id']} text: {update['message']['text']}") ###
+			if self.last_message_id < update['update_id']:
+				self.last_message_id = update['update_id']
+			print(self.last_message_id)
+			print(update['message']['chat']['id'])
+			print(update['message']['chat']['first_name'])
+			print(update['message']['chat']['username'])
+			data = f"Привет, {update['message']['chat']['first_name']}! Знаешь ли ты, что твой ник в телеге {update['message']['chat']['username']}?"
+			self.send_result(update['message']['chat']['id'], data)
+			#sendMessage(update['message']['chat']['id'], f"Привет, {update['message']['chat']['first_name']}! Ты знаешь, что твой ник в телеге {update['message']['chat']['username']}")
+					
+	#---------в разработке
 	def run(self):
 		"""Главный цикл сбора данных"""
 		try:            
             # Запуск фонового потока
 			exit_thread = threading.Thread(target=self.wait_for_exit, daemon=True)
 			exit_thread.start()
-
-			session = requests.session() 
-			adapter = HTTPAdapter(
-				pool_connections = POOL_CONNECTIONS,
-				pool_maxsize = POOL_MAXSIZE,
-				max_retries = MAX_RETRIES,
-				pool_block = POOL_BLOCK
-			)
-			session.mount("https://", adapter)
-			url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+						
+			url_post = f"https://api.telegram.org/bot{self.token}/sendMessage"					
 			# параметры для запроса к боту
 			params = {
-				'timeout': 30, # время ожидания новой команды (лонг пуллинг)
-				'limit': 100   # ограничение количества сообщений в одном ответе
+				'timeout': POLL_INTERVAL, 	# время ожидания новой команды (лонг пуллинг)
+				'limit': LIMIT 				# ограничение количества сообщений в одном ответе
 			}
+
 			# Получение сообщений
-			while not self.exit_flag:				
-				if self.last_message_id:
-					params['offset'] = self.last_message_id + 1 # для чтения только необработанных команд
-				response = session.request("get", url, params=params, timeout=35)
-				print("get")
-				updates = response.json()['result']
-				#print(updates[1].get("message").get("message_id"))
-				print(updates)
-				self.pause()
+			while not self.exit_flag:
+				print("start")				
+				#print(self.last_message_id)				
+				updates = self.get_updates(params)
+				#print(f"updates={updates}")
+				if updates:
+					print("новое сообщение")
+					self.messages_processing(updates)
+
+
+
+
+
+				#self.pause()
+				print("end")
 		except Exception as e:
 			print(f"Ошибка: {e}")
 		finally:
-			session.close()
+			self.session.close()
 			print("Сессия закрыта")
 
-test = TelegramBot(token=TELEGRAM_TOKEN, poll_interval=20)
+token = '' 
+test = TelegramBot(token)
 test.run()
